b15ae7c9522fb3014e641b04b5faec0a7b24b12f
sait-357 <22r01a6616@gmail.com>
2025-04-20 23:18:37 +0530

    created stage 2, modified stage 1, and App.js files

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from "./components/Home";
 import Login from "./components/Login"; 
 import Dashboard  from "./components/Dashboard";// Import the Login page
 import Stage1 from "./components/stage1";
+import Stage2 from "./components/stage2";
 function App() {
   return (
     <Router>
@@ -14,6 +15,7 @@ function App() {
         <Route path="/login" element={<Login />} /> {/* Add the login route */}
         <Route path ="/dashboard" element={<Dashboard/>}/>
         <Route path ="/stage1" element={<Stage1 />}/>
+        <Route path ="/stage2" element={<Stage2 />}/>
       </Routes>
     </Router>
   );
